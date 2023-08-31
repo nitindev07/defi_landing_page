828a7fac328dd5adda2bf3ceb22d52f49882223b
Nitin Singh <143168728+nitindev07@users.noreply.github.com>
2023-08-31 15:56:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Developer from './components/Developer';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
-import Pappu from './components/Pappu';
 
 function App() {
   return (
